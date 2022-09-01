4aed256d44a59f115408802b34ea7c7f9e6806ef
Juan Carlos <jcvargas273@gmail.com>
2022-09-01 10:47:09 +0100

    nuevo PORT

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(
     }
 );
 
-const PORT =  process.env.PORT || 3000;
+const PORT =  process.env.PORT;
 app.listen(PORT =>{
     console.log(`El servidor esta levantado en el puerto ${PORT}`)
 });
