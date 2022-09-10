7516b7c6a3b0256bd64d6d542d6f5c06cdb19b63
jormais <jonaybrito15@gmail.com>
2022-09-10 14:12:50 +0100

    se ha añadido Json Web Token, verificaciones en schema y un middleware para verificar el token

--- api/users/users.model.js
+++ api/users/users.model.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    pswd : String,
+    username:{
+        type: String,
+        required:'Es necesario un nombre de usuario',
+        maxlength:[8,"El nombre de usuario debe tener un maximo de 8 caracteres"],
+        minlength:[3, "El nombre de usuario debe tener un minimo de 3 caracteres"],
+        unique:true
+    },
+    email: {
+        type: String,
+        required:'Es necesario un email',
+        maxlength:[30,"El email tiene un maximo permitido de 30 caracteres"],
+        unique:true
+    },
+    pswd : {
+        type: String,
+        required:'Es necesario una contraseña',
+        minlength:[6, "La contraseña debe tener un minimo de 6 caracteres"]
+    },
     favorites : [String]
 });
 
