c2e2cf856b67fef1fe8939cca89c90ab056868a9
jormais <jonaybrito15@gmail.com>
2022-09-11 20:35:25 +0100

    añadido endpoind login y auth

--- api/auth/authUser.js
+++ api/auth/authUser.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 const User = require('../users/users.model');
 
-
-function authUser(req, res) {
-    User.findOne({email : req.body.email, password: req.body.password })
+function loginUser(req, res) {
+    console.log("ENTRO");
+    User.findOne({email : req.query.email, password: req.query.pswd })
     .then( userFound =>  {
             let token = jwt.sign({usr :userFound.email, psw : userFound.pswd}, process.env.SECRET);
             res.send({
@@ -15,4 +15,13 @@ function authUser(req, res) {
         
 }
 
-module.exports = authUser;
+function authUser(req, res){
+    console.log(req.user);
+    if(req.user){
+        res.send(true)
+    }
+}
+
+
+
+module.exports = {loginUser, authUser};
