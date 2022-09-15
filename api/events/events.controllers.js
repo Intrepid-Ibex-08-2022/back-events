77763e027a4a5ae9d1a8d45f11421d22ecbde8ad
jormais <jonaybrito15@gmail.com>
2022-09-15 18:11:35 +0100

    eliminado bug en paginacion

--- api/events/events.controllers.js
+++ api/events/events.controllers.js
@@ -12,11 +12,12 @@ function getAll(req, res) {
       if (paginNum) {
         let paginatedEvents = [];
         found.forEach((event, index) => {
-          if (index >= paginNum * 10 - 11 && index <= paginNum * 10 - 1) {
+          if (index >= paginNum * 10 - 10 && index <= paginNum * 10 - 1) {
             paginatedEvents.push(event);
           }
         });
         res.send(paginatedEvents);
+        console.log(paginatedEvents.length);
       } else {
         res.send(found);
       }
