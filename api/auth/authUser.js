82046313f30d4ec0f997a85c6b18b15852d98278
Juan Carlos <jcvargas273@gmail.com>
2022-09-15 10:18:02 +0100

    corregido endpoint login, para recibir parametros por body

--- api/auth/authUser.js
+++ api/auth/authUser.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../users/users.model');
 
 function loginUser(req, res) {
-    User.findOne({email : req.query.email, password: req.query.pswd })
+    User.findOne({email : req.body.email, password: req.body.pswd })
     .then( userFound =>  {
             let token = jwt.sign({usr :userFound.username, mail : userFound.email, fav : userFound.favorites}, process.env.SECRET);
             res.send({
